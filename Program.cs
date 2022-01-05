8c8226fdc838c833e045d2d4fcfdd85d1087c653
Saeed Nemati <sn.1361@gmail.com>
2022-01-05 15:07:29 +0330

    committed by script

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
 ﻿global using Holism.Infra;
 global using System.Text.RegularExpressions;
+global using Holism.DataAccess;
+global using System.Data;
 
 var connection = InfraConfig.GetConnectionString("Accounts");
 var masterConnection = Regex.Replace(connection, @"database=.*", "");
-Logger.LogInfo(masterConnection);
+// Logger.LogInfo(masterConnection);
+var databases = Database.Open(masterConnection).Get("show databases");
+foreach (DataRow database in databases.Rows)
+{
+    var databaseName = database["Database"].ToString();
+    if (Char.IsLower(databaseName[0])) 
+    {
+        continue;
+    }
+    Logger.LogInfo(databaseName);
+}
