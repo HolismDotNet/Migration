20a0428ebd5f49913068537e013267012fe18e33
Saeed Nemati <sn.1361@gmail.com>
2022-01-10 07:56:36 +0330

    committed by script

--- TableHandler.cs
+++ TableHandler.cs
@@ -5,7 +5,7 @@ public class TableHandler : Handler
         var query = @$"
             show create table `{DatabaseName}`.`{TableName}`
         ";
-        var creationScript = Database.Open(MasterConnection).Get(query).Rows[0][0].ToString();
+        var creationScript = Database.Open(MasterConnection).Get(query).Rows[0][1].ToString();
         File.WriteAllText(Path.Combine(Dir, $"{TableFqn}.sql"), creationScript);
         // new ColumnHandler().Handle();
         // new ForeignKeyHandler().Handle();
