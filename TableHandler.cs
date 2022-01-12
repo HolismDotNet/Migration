ce4c1a53a495ba25de4bae243d58d686eebae4f1
Saeed Nemati <sn.1361@gmail.com>
2022-01-12 09:14:00 +0330

    committed by script

--- TableHandler.cs
+++ TableHandler.cs
@@ -6,6 +6,7 @@ public class TableHandler : Handler
             show create table `{DatabaseName}`.`{TableName}`
         ";
         var creationScript = Database.Open(MasterConnection).Get(query).Rows[0][1].ToString();
+        creationScript = Regex.Replace(creationScript, @"ENGINE=InnoDB.*DEFAULT", "ENGINE=InnoDB DEFAULT");
         File.WriteAllText(Path.Combine(Dir, $"{TableFqn}.sql"), creationScript);
         // new ColumnHandler().Handle();
         // new ForeignKeyHandler().Handle();
